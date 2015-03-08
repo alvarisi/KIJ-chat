6764e3d88f36fb4077dc1b8d4d11393191a23ee3
andrejoshua <andrew.n12@mhs.if.its.ac.id>
2015-03-09 06:08:53 +0700

    server.c (belum selesai)
    - no multithreading yet
    - just server as chat user

---
+++ server.c
@@ -0,0 +1,85 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+     int sockfd, newsockfd, portno;
+     socklen_t clilen;
+     char buffer[256];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0)
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0)
+              error("ERROR on binding");
+     listen(sockfd,5);
+
+     while(1){
+        clilen = sizeof(cli_addr);
+        newsockfd = accept(sockfd,(struct sockaddr *) &cli_addr, &clilen);
+        if (newsockfd < 0) error("ERROR on accept");
+        bzero(buffer,256);
+        n = read(newsockfd,buffer,255);
+        if (n < 0) error("ERROR reading from socket");
+
+        char code[5];
+        int i;
+        for(i=0; i<strlen(code)-1; i++){
+            if(buffer[i]==' ' || buffer[i]=='\0'){
+                code[i] = '\0';
+                i += 1;
+                break;
+            }
+            code[i] = buffer[i];
+        }
+
+        if(strcmp(code,"send")==0){
+            char message[256];
+            if(i<strlen(buffer)){
+                int j;
+                for(j=i;j<strlen(buffer)-1;j++){
+                    message[j] = buffer[j];
+                }
+                printf("Client message: %s\n", message);
+            }
+            else{
+                printf("Client sent no message\n");
+            }
+            char msg[100];
+            scanf("%s",msg);
+            n = write(newsockfd,msg,strlen(msg));
+            if (n < 0) error("ERROR writing to socket");
+        }
+        else if(strcmp(code,"end")==0){
+            sleep(2);
+            close(newsockfd);
+            close(sockfd);
+            break;
+        }
+     }
+     return 0;
+}
