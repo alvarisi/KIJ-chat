5f45ba969027d4b752f8b97aeb9074d8c13b79d3
alvarisi <alvarisi01@gmail.com>
2015-03-10 20:33:39 +0700

    Lanjutkan

--- chatUI.py
+++ chatUI.py
@@ -1,5 +1,30 @@
 from Tkinter import *
 import tkMessageBox
+import socket
+import select
+import sys
+import json
+import time
+import thread
+import os
+server_address = (ipaddress, int(port))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(server_address)
+BUFF = 1024
+
+try:
+	while 1:
+		socket_list = [sys.stdin, client_socket]
+		# Get the list sockets which are readable
+		ready_to_read, ready_to_write, in_error = select.select(socket_list , [], [])
+		for sock in ready_to_read: 
+			if sock == client_socket: #receive
+				#recvhandler(sock)
+				#sys.stdout.flush()
+				#sys.stdout.write('>> ')
+			else:	
+				#sys.stdout.write('>> ')	
+				#typehandler()
 
 root = Tk(  )
 
